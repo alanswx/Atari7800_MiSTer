2d2a3bb52f46debc99e284ea56045e87f28775f5
Alan Steremberg <alan.steremberg@gmail.com>
2019-09-03 16:29:20 -0700

    trying to unify ram

--- sim/spram.sv
+++ sim/spram.sv
@@ -16,6 +16,6 @@ module spram #(parameter addr_width=8, parameter data_width=8)
 	//$display("data=%h addr=%h word=%h",data, addr, addr[31:2]);
 	end
 
-	assign q= cs ? mem[address] : {data_width{1'b1}};;
+	assign q= cs ? mem[address] : {data_width{1'b1}};
 
 endmodule: spram
